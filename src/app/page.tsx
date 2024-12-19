ebead74798a6cdcbb3b31b676a54979b13f20939
PrasathRavichandran <prasath.webby@gmail.com>
2024-12-19 21:46:20 +0530

    - navbar ui completed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Navbar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <Button> Click me!!</Button>
+      <Navbar />
     </main>
   );
 }
